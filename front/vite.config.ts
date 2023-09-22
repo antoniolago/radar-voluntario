f05f9c5c614fbbbdd29f5a5ea585b14b8381c8d7
Gustavo Harff <gustavo.harff@gmail.com>
2023-09-22 20:51:01 -0300

    chore: Try run front-end with https

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+import mkcert from 'vite-plugin-mkcert'
 import dotenv from 'dotenv'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 dotenv.config({ path: '../.env' })
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), mkcert()],
   envDir: '../',
   resolve: {
     alias: [
