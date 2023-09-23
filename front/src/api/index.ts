ceee1605bdf5f72adba3b9b0c1f529c4eece5c07
Gustavo Harff <gustavo.harff@gmail.com>
2023-09-22 21:22:40 -0300

    chore: Use VITE_API_URL

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,20 +1,25 @@
+import { CONFIG } from '@/configs';
 import axios from 'axios';
 export const api = {
     get: <T>(url: string, params?: object) =>
       axios.get<T>(url, {
         withCredentials: true,
+        baseURL: CONFIG.API_URL,
         ...params,
       }),
     post: <T>(url: string, data: any) =>
       axios.post<T>(url, data, {
         withCredentials: true,
+        baseURL: CONFIG.API_URL,
       }),
     patch: <T>(url: string, data: any) =>
       axios.patch<T>(url, data, {
         withCredentials: true,
+        baseURL: CONFIG.API_URL,
       }),
     delete: <T>(url: string) =>
       axios.delete<T>(url, {
         withCredentials: true,
+        baseURL: CONFIG.API_URL,
       }),
   };
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from "path"
 import mkcert from 'vite-plugin-mkcert'
-import dotenv from 'dotenv'
-
-dotenv.config({ path: '../.env' })
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
-  envDir: '../',
   resolve: {
     alias: [
       { find: 'react-query/devtools', replacement: 'react-query/es/devtools/index'},
@@ -19,6 +15,6 @@ export default defineConfig({
   server: {
     host: true,
     https: true,
-    port: +process.env.FRONTEND_PORT
+    port: Number(process.env.FRONTEND_PORT)
   }
 })
