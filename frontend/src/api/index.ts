1dd875e016a6b4ff129ac21abca723adfae7253d
antoniolago <45375617+antoniolago@users.noreply.github.com>
2023-10-07 21:28:40 -0300

    static api url

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,21 +1,22 @@
 import { CONFIG } from '@/configs';
 import axios from 'axios';
+const API_URL = CONFIG.API_URL+"/api";
 export const api = {
     get: <T>(url: string, params?: object) =>
       axios.get<T>(url, {
-        baseURL: CONFIG.API_URL,
+        baseURL: API_URL,
         ...params,
       }),
     post: <T>(url: string, data: any) =>
       axios.post<T>(url, data, {
-        baseURL: CONFIG.API_URL,
+        baseURL: API_URL,
       }),
     patch: <T>(url: string, data: any) =>
       axios.patch<T>(url, data, {
-        baseURL: CONFIG.API_URL,
+        baseURL: API_URL,
       }),
     delete: <T>(url: string) =>
       axios.delete<T>(url, {
-        baseURL: CONFIG.API_URL,
+        baseURL: API_URL,
       }),
   };
