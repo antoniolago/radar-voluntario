2650e2fe436db02d82d56b1238b0767a0335f8b1
antoniolago <45375617+antoniolago@users.noreply.github.com>
2023-10-08 21:50:38 -0300

    fix api url on local env

--- frontend/src/components/MapComponent/index.tsx
+++ frontend/src/components/MapComponent/index.tsx
@@ -1,12 +1,14 @@
+import { TemaContext } from '@/contexts/Tema';
 import { LatLngExpression, Map } from 'leaflet';
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function MapComponent() {
+  const { isDarkTheme } = useContext(TemaContext);
   const mapRef = useRef<Map>();
   const position: any = [-30.03306, -51.23];
   return (
-    <div id="map">
+    <div id="map" className={isDarkTheme ? "dark" : "light"}>
       <MapContainer ref={mapRef as any} center={position as LatLngExpression} zoom={14} scrollWheelZoom={true} style={{ height: "80vh" }} >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
