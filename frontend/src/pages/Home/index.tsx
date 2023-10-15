8adf125e8cb9cc406a488e7a5cd8ef4b765c0b88
antoniolago <45375617+antoniolago@users.noreply.github.com>
2023-10-14 22:49:28 -0300

    test browser tab hiding with scrollTo

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
 import MapComponent from '@/components/MapComponent';
 import Box from '@mui/joy/Box/Box';
+import { useEffect } from 'react';
 
 function Home() {
+    useEffect(() => {
+        window.scrollTo(0, 1);
+    })
     return (
         <Box sx={{width: '100%', height: '100%'}}>
             <MapComponent />
