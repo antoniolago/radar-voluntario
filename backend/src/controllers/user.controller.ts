69bd29d4cd828821c03d73f60b2cbd924296da7e
Gustavo Harff <gustavo.harff@gmail.com>
2023-10-18 23:58:02 +0000

    Update user.controller.ts

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -7,6 +7,6 @@ export class UserController {
   public index = async (request: Request, response: Response) => {
     const user = await this.accountsService.getUser(request.user.email);
     
-    return response.json(json);
+    return response.json(user);
   }
 }
