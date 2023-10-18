f55a5dc352e9b329ca6cce64075401d5d58033bb
Gustavo Harff <gustavo.harff@gmail.com>
2023-10-18 23:54:47 +0000

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -62,7 +62,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 
   return response.status(500).json({
     status: "error",
-    message: error?.toString(),
+    message: err?.toString(),
   });
 });
 
