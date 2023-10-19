9a2f4558d42202476ce50f4432eaa0d4f94a13f6
Brenda Barbosa <brendabarbosa@rede.ulbra.br>
2023-10-19 20:26:44 -0300

    Add dialogs to account settings

---
+++ frontend/src/components/AlertDialog/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AlertDialog(props: any) {
+
+  const handleClose = () => {
+    props.setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {props.title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {props.description}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="error" variant="contained" onClick={handleClose}>Cancelar</Button>
+          <Button color="success" variant="contained" onClick={props.handleClick} autoFocus>
+            {props.confirmText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
