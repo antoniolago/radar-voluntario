4845d0662c5724b1a147c158e1dbb24840572d40
Gustavo Harff <gustavo.harff@gmail.com>
2023-10-27 07:31:17 -0300

    feat: Add zip_code and require latitude and longitude

--- backend/src/services/institutions.service.ts
+++ backend/src/services/institutions.service.ts
@@ -1,18 +1,12 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "../database/prisma";
-import axios from "axios";
 import AppError from "../errors/app-error";
 
 type OptionalFields = "id";
 
 type SaveCommand = Omit<Prisma.InstitutionCreateManyInput, OptionalFields>;
 
-type OptionalAddressFields =
-  | "id"
-  | "latitude"
-  | "longitude"
-  | "institution"
-  | "institution_id";
+type OptionalAddressFields = "id" | "institution" | "institution_id";
 
 type AddressSaveCommand = Omit<
   Prisma.InstitutionAddressCreateManyInput,
@@ -73,19 +67,6 @@ export class InstitutionsService {
     institutionId: string,
     command: AddressSaveCommand
   ) => {
-    const response = await axios.get("https://geocode.maps.co/search", {
-      params: {
-        q: `${command.street}, ${command.number} - ${command.city}, ${command.state}`,
-      },
-      maxRedirects: 5,
-    });
-
-    const geocodeAddress = response.data[0];
-
-    if (!geocodeAddress) {
-      throw new AppError("Geocode not found", 404);
-    }
-
     const institution = await prisma.institution.findUnique({
       where: {
         id: institutionId,
@@ -99,8 +80,6 @@ export class InstitutionsService {
     const address = await prisma.institutionAddress.create({
       data: {
         ...command,
-        latitude: Number(geocodeAddress.lat),
-        longitude: Number(geocodeAddress.lon),
         institution: {
           connect: {
             id: institutionId,
