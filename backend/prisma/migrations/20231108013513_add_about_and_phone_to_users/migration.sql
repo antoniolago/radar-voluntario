313f6cc8163db0fe48461f340b4d4a061c86cbd9
Gustavo Harff <gustavo.harff@gmail.com>
2023-11-07 22:43:30 -0300

    feat: Add about and phone on user

---
+++ backend/prisma/migrations/20231108013513_add_about_and_phone_to_users/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "about" TEXT,
+ADD COLUMN     "phone" TEXT;
--- backend/src/services/accounts.service.ts
+++ backend/src/services/accounts.service.ts
@@ -4,6 +4,11 @@ import jsonwebtoken from "jsonwebtoken";
 import { prisma } from "../database/prisma";
 import AppError from "../errors/app-error";
 
+interface UpdateAccountParams {
+  about?: string;
+  phone?: string;
+}
+
 export class AccountsService {
   public async getUser(email: string){
     return await prisma.user.findUnique({
@@ -74,5 +79,26 @@ export class AccountsService {
     );
 
     return { user, token };
+  }
+
+  public async updateUser(data: UpdateAccountParams, userId: string) {
+    const user = await prisma.user.update({
+      where: { id: userId },
+      data,
+    });
+
+    if (!user) {
+      throw new AppError("User not found", 404);
+    }
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: {
+        about: data.about,
+        phone: data.phone,
+      },
+    })
+
+    return user;
   }
 }
